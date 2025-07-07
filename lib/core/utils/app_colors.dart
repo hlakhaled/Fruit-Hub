a8e476812fa0581abd8a03efb890cf57309fa7a2
HlaKhaled <hlakhaled647@gmail.com>
2025-07-07 22:26:32 +0300

    add app colors

---
+++ lib/core/utils/app_colors.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  // Green Shades
+  static const Color green50 = Color(0xFFEBF9F1);
+  static const Color green200 = Color(0xFFB0E8C7);
+  static const Color green400 = Color(0xFF60D290);
+  static const Color green500 = Color(0xFF5DB957);
+  static const Color green600 = Color(0xFF2D9F5D);
+  static const Color green800 = Color(0xFF21481E);
+  static const Color greenMain = Color(0xFF53B175);
+
+  // Grayscale
+  static const Color gray950 = Color(0xFF0C0D0D);
+  static const Color gray600 = Color(0xFF616A6B);
+  static const Color gray500 = Color(0xFF4E5556);
+  static const Color gray400 = Color(0xFF949D9E);
+  static const Color gray200 = Color(0xFFCACECE);
+  static const Color gray50  = Color(0xFFF2F3F3);
+
+  // Black and White
+  static const Color black = Color(0xFF000000);
+  static const Color white = Color(0xFFFFFFFF);
+
+  // Yellow/Orange
+  static const Color orange500 = Color(0xFFF4A91F);
+  static const Color orange200 = Color(0xFFFADA9E);
+
+  // Accent Colors
+  static const Color accentYellow = Color(0xFFFFC529);
+  static const Color accentRed = Color(0xFFF24135);
+  static const Color accentBlue = Color(0xFF4285F4);
+  static const Color accentGreen = Color(0xFF34A853);
+
+  // Misc
+  static const Color shadowGray = Color(0xFF6B6B6B);
+  static const Color lightBackground = Color(0xFFF9F9FA);
+}
